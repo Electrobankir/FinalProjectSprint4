a2d90249e926a2d0ca0cc9058aa52b17d46ed2ec
Project4 <f1marsel@gmail.com>
2022-11-17 18:27:35 +0300

    My first commit

---
+++ src/main/java/ru/yandex/practikum/OrderPage.java
@@ -0,0 +1,26 @@
+package ru.yandex.practikum;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class OrderPage {
+    private final WebDriver driver;
+
+    public OrderPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Локатор для кнопки заказать
+    //private final static By ORDER =
+    //Локатор для поля <Имя>
+    //Локатор для поля <Фамилия>
+    //Локатор для поля <Адрес>
+    //Локатор для выбора станции метро
+    //Локатор для поля <Телефон>
+    //Локатор для поля <Когда привезти самокат>
+    //Локатор для поля <Срок аренды>
+    //Локатор для выбора цвета самоката
+    //Локатор для поля <Комментарий>
+    //Локатор для кнопки <Да> при подтверждении заказа
+    //Локатор для кнопки <Нет> при подтверждении заказа
+    //Локатор для поля <Заказ оформлен>
+}
